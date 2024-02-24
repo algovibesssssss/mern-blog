3b937f72f62342efb735a8be0f7c8497e6881241
algovibesssssss <161154517+algovibesssssss@users.noreply.github.com>
2024-02-25 01:08:36 +0530

    Update CreatePost.js

--- client/my-app/src/Pages/CreatePost.js
+++ client/my-app/src/Pages/CreatePost.js
@@ -20,7 +20,7 @@ export default function CreatePost(){
 
         ev.preventDefault();
         
-        const response = await fetch('http://localhost:4000/post',{
+        const response = await fetch('https://api-seven-psi.vercel.app/post',{
            method: 'POST',
             body: data,
             credentials: 'include',
