78eb896e4e2467428e464b042c93cb144c0bb6bd
algovibesssssss <161154517+algovibesssssss@users.noreply.github.com>
2024-02-25 01:01:56 +0530

    Update CreatePost.js

--- client/my-app/src/Pages/CreatePost.js
+++ client/my-app/src/Pages/CreatePost.js
@@ -20,7 +20,7 @@ export default function CreatePost(){
 
         ev.preventDefault();
         
-        const response = await fetch('https://new-mern-back.vercel.app/post',{
+        const response = await fetch('http://localhost:4000/post',{
            method: 'POST',
             body: data,
             credentials: 'include',
