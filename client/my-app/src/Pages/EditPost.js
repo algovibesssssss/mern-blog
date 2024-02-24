7aaf62a96db0ef13fa05082d159e7406044d1263
algovibesssssss <161154517+algovibesssssss@users.noreply.github.com>
2024-02-25 00:50:25 +0530

    Update EditPost.js

--- client/my-app/src/Pages/EditPost.js
+++ client/my-app/src/Pages/EditPost.js
@@ -13,7 +13,7 @@ export default function EditPost(){
     const[files,setFiles] = useState('');
     const[redirect,setRedirect]=useState(false);
     useEffect(()=>{
-        fetch('http://localhost:4000/post/'+id)
+        fetch('https://new-mern-back.vercel.app/'+id)
             .then(response=>{
                 response.json().then(postInfo=>{
                     setTitle(postInfo.title);
