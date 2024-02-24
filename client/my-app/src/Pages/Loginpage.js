d0d583d646b90a456d92835976303ed7cdc13d49
algovibesssssss <161154517+algovibesssssss@users.noreply.github.com>
2024-02-25 01:02:52 +0530

    Update Loginpage.js

--- client/my-app/src/Pages/Loginpage.js
+++ client/my-app/src/Pages/Loginpage.js
@@ -11,7 +11,7 @@ export default function Loginpage(){
 
     async function login(ev){
         ev.preventDefault();
-        const response = await fetch('https://new-mern-back.vercel.app/login',{
+        const response = await fetch('http://localhost:4000/login',{
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'Content-Type':'application/json'},
