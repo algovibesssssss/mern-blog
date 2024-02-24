849d2c4e09f52fc1deb9a09c1dc1423aa0cb8847
algovibesssssss <161154517+algovibesssssss@users.noreply.github.com>
2024-02-25 01:34:01 +0530

    Update post.js

--- client/my-app/src/post.js
+++ client/my-app/src/post.js
@@ -7,7 +7,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
         <div className="post">
         <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://new-mern-back.vercel.app/'+cover} alt="" />
+          <img src={'https://api-seven-psi.vercel.app/'+cover} alt="" />
         </Link>
         </div>
 
